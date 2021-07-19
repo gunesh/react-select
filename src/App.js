de0c119f2c0563a4b0fae1571dbac362c23db6a6
gunesh <gunesh26@gmail.com>
2021-07-19 12:45:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Select from 'react-select';
+
+import './style.css';
+const options = [
+  { value: 'chocolate', label: 'Chocolate' },
+  { value: 'strawberry', label: 'Strawberry' },
+  { value: 'vanilla', label: 'Vanilla' }
+];
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <Select
+        className="basic-single"
+        classNamePrefix="select"
+        defaultValue={options[0]}
+        isDisabled={false}
+        isLoading={false}
+        isClearable={true}
+        isRtl={false}
+        isSearchable={true}
+        name="color"
+        options={options}
+      />
+    </div>
+  );
+}
