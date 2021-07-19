7c0ecc819a4e3c89d7b99c81a0930fbe2fb6724c
gunesh <gunesh26@gmail.com>
2021-07-19 14:07:28 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Select from 'react-select';
 
 import './style.css';
 const options = [
-  { value: 'chocolate1', label: 'Chocolate1',taxid:['a','b','c'] },
-  { value: 'chocolate', label: 'Chocolate',taxid:['a1','b1','c1'] },
-  { value: 'strawberry', label: 'Strawberry',taxid:['a2','b2','c2'] },
-  { value: 'vanilla', label: 'Vanilla',taxid:['a3','b3','c3'] }
+  { value: 'chocolate1', label: 'Chocolate1', taxid: ['a', 'b', 'c'] },
+  { value: 'chocolate', label: 'Chocolate', taxid: ['a1', 'b1', 'c1'] },
+  { value: 'strawberry', label: 'Strawberry', taxid: ['a2', 'b2', 'c2'] },
+  { value: 'vanilla', label: 'Vanilla', taxid: ['a3', 'b3', 'c3'] }
 ];
 export default function App() {
   return (
